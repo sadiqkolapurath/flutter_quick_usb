914ae0af9a13af06c16ed54cb69e8f8c21926063
sadiq <sadiq@lithos.in>
2025-03-04 11:03:25 +0530

    Linux class miss match bug fidex

--- example/linux/flutter/generated_plugin_registrant.cc
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -6,10 +6,10 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <flutter_quick_usb/quick_usb_plugin.h>
+#include <flutter_quick_usb/flutter_quick_usb_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) flutter_quick_usb_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "QuickUsbPlugin");
-  quick_usb_plugin_register_with_registrar(flutter_quick_usb_registrar);
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterQuickUsbPlugin");
+  flutter_quick_usb_plugin_register_with_registrar(flutter_quick_usb_registrar);
 }
